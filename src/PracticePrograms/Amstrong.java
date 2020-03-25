907ebf377b3b39a8ac58c83313dff8e7593edf32
Sunita <sunita@Sunitas-MacBook-Air.local>
2020-03-25 20:21:50 +0530

    Amstrong program changed.

--- src/PracticePrograms/Amstrong.java
+++ src/PracticePrograms/Amstrong.java
@@ -5,11 +5,6 @@ import java.util.Scanner;
 public class Amstrong {
 
 	public static void main(String[] args) {
-		
-		int no;
-		Scanner sc = new Scanner(System.in);
-		no = sc.nextInt();
-		System.out.println("Enter the number: "+no);
 	
 
 	}
